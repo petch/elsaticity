dbb8314dd8c38856ad541aaf899a315c116c384d
Petr Zakharov <petr.e.zakharov@itwm.fraunhofer.de>
2018-08-08 13:14:18 +0200

    update

--- composite.py
+++ composite.py
@@ -1,6 +1,7 @@
 from dolfin import *
 from mshr import *
 import matplotlib.pyplot as plt
+import math
 
 def Fiber(w, h, x, y):
     domain = Rectangle(Point(x - w/2, y - h/2), Point(x + w/2, y + h/2))
@@ -26,86 +27,107 @@ def Press(w, h, rx, ry, s):
            - Rectangle(Point(-w/2, -h), Point(-w/2 + rx, -ry)) \
            - Rectangle(Point(w/2 - rx, -h), Point(w/2, -ry))
     return domain
-    
-def Block(w, h, fw, fh, dx, dy, p, pw):
-    presses = CSGTranslation(p, Point(pw/2, 0)) \
-            + CSGTranslation(p, Point(w-pw/2, 0)) \
-            + CSGTranslation(CSGRotation(p, DOLFIN_PI), Point(w/2, h))
+
+def PressTop(w, h, rx, ry, s):
+    domain = Rectangle(Point(-w/2, 0), Point(w/2, h)) \
+           - Ellipse(Point(-w/2, h-ry), rx, ry, s) \
+           - Ellipse(Point(w/2, h-ry), rx, ry, s) \
+           - Rectangle(Point(-w/2, ry), Point(-w/2 + rx, h)) \
+           - Rectangle(Point(w/2 - rx, ry), Point(w/2, h))
+    return domain
+
+def Block(w, h, fw, fh, dx, dy, p, pt, sw):
+    presses = CSGTranslation(p, Point(sw/2, 0)) \
+            + CSGTranslation(p, Point(w-sw/2, 0)) \
+            + CSGTranslation(pt, Point(w/2, h))
     domain = Composite(w, h, fw, fh, dx, dy, presses)
     return domain
 
-def CoarseBlock(w, h, p, pw):
-    presses = CSGTranslation(p, Point(pw/2, 0)) \
-            + CSGTranslation(p, Point(w-pw/2, 0)) \
-            + CSGTranslation(CSGRotation(p, DOLFIN_PI), Point(w/2, h))
+def CoarseBlock(w, h, p, pt, sw):
+    presses = CSGTranslation(p, Point(sw/2, 0)) \
+            + CSGTranslation(p, Point(w-sw/2, 0)) \
+            + CSGTranslation(pt, Point(w/2, h))
     domain = Rectangle(Point(0, 0), Point(w, h)) + presses
     domain.set_subdomain(1, presses)
     return domain
 
-def SparseBlock(w, h, fw, fh, dx, dy, p, pw, rvew, rveh):
-    p = p + Rectangle(Point(-pw/2, 0), Point(pw/2, rveh))
-    presses = CSGTranslation(p, Point(pw/2, 0)) \
-            + CSGTranslation(p, Point(w-pw/2, 0)) \
-            + CSGTranslation(CSGRotation(p, DOLFIN_PI), Point(w/2, h))
+def SparseBlock(w, h, fw, fh, dx, dy, p, pt, sw, swt, sh):
+    p = p + Rectangle(Point(-sw/2, 0), Point(sw/2, sh))
+    pt = pt + Rectangle(Point(-swt/2, -sh), Point(swt/2, 0))
+    presses = CSGTranslation(p, Point(sw/2, 0)) \
+            + CSGTranslation(p, Point(w-sw/2, 0)) \
+            + CSGTranslation(pt, Point(w/2, h))
     domain = Rectangle(Point(0, 0), Point(w, h)) + presses
     domain.set_subdomain(1, presses)
     nx = int(w / dx)
     ny = int(h / dy)
     for i in range(nx):
-        x = w * (i + 0.5) / nx 
+        x = w * (i + 0.5) / nx
         for j in range(ny):
             y = h * (j + 0.5) / ny
-            if near(x, pw/2, pw/2) and near(y, 0, rveh) \
-            or near(x, w-pw/2, pw/2) and near(y, 0, rveh) \
-            or near(x, w/2, pw/2) and near(y, h, rveh):
+            if near(x, sw/2, sw/2) and near(y, 0, sh) \
+            or near(x, w-sw/2, sw/2) and near(y, 0, sh) \
+            or near(x, w/2, sw/2) and near(y, h, sh):
                 domain.set_subdomain(i * ny + j + 2, Fiber(fw, fh, x, y))
     return domain
 
-def save(geo, name, resolution, do_plot=False):
+def save(geo, name, resolution, refineTimes=0):
     print(name)
     mesh = generate_mesh(geo, resolution)
     domains = MeshFunction("size_t", mesh, 2, mesh.domains())
+    for r in range(refineTimes):
+        need_refine = MeshFunction("bool", mesh, 2, False)
+        for c in cells(mesh):
+            if domains[c] > 0:
+                need_refine[c] = True
+        mesh = refine(mesh, need_refine)
+        domains = adapt(domains, mesh)
     File("meshes/" + name + ".xml") << mesh
     File("meshes/" + name + "_domains.xml") << domains
     File("meshes/" + name + "_domains.pvd") << domains
-    if do_plot:
-        plt.figure(figsize=(16, 12))
-        plot(mesh)
-        plot(domains)
+    plt.figure(figsize=(16, 12))
+    plot(mesh)
+    plot(domains)
     
 
 fiberWidth  = 0.05
-fiberHeight = 0.025
+fiberHeight = 0.0125
 densityX    = 0.1
 densityY    = 0.1
 rveWidth    = 0.1
 rveHeight   = 0.1
 blockWidth  = 2.0
 blockHeight = 0.5
-pressWidth  = 0.4
+pressWidth  = 0.3
 pressHeight = 0.1
 roundX      = 0.05
 roundY      = 0.05
 resolution  = 40
 coarse      = 20
 segments    = 8
+refineTimes = 1
+sparseWidth = 5*rveWidth
+sparseTop   = 6*rveWidth
 
 #parameters["lloyd_optimize"] = True
 #parameters["mesh_resolution"] = resolution
 #parameters["edge_truncate_tolerance"] = 1e-16
 
 rve = Composite(rveWidth, rveHeight, fiberWidth, fiberHeight, densityX, densityY)
-save(rve, "rve", sqrt(rveWidth*rveHeight)*resolution, True)
+save(rve, "rve", sqrt(rveWidth*rveHeight)*resolution)
 
 press = Press(pressWidth, pressHeight, roundX, roundY, segments)
-save(press, "press", sqrt(pressWidth*pressHeight)*resolution, True)
+pressTop = PressTop(pressWidth, pressHeight, roundX, roundY, segments)
+save(press, "press", sqrt(pressWidth*pressHeight)*resolution)
+save(pressTop, "pressTop", sqrt(pressWidth*pressHeight)*resolution)
 
-block = Block(blockWidth, blockHeight, fiberWidth, fiberHeight, densityX, densityX, press, pressWidth)
-save(block, "block", sqrt(blockWidth*blockHeight)*resolution, True)
+block = Block(blockWidth, blockHeight, fiberWidth, fiberHeight, densityX, densityY, press, pressTop, sparseWidth)
+save(block, "block", sqrt(blockWidth*blockHeight)*resolution)
 
-block = CoarseBlock(blockWidth, blockHeight, press, pressWidth)
-save(block, "coarse", sqrt(blockWidth*blockHeight)*coarse, True)
+block = CoarseBlock(blockWidth, blockHeight, press, pressTop, sparseWidth)
+save(block, "coarse", sqrt(blockWidth*blockHeight)*coarse)
 
-sparse = SparseBlock(blockWidth, blockHeight, fiberWidth, fiberHeight, densityX, densityX, press, pressWidth, rveWidth, rveHeight)
-save(sparse, "sparse", sqrt(blockWidth*blockHeight)*coarse, True)
+block = SparseBlock(blockWidth, blockHeight, fiberWidth, fiberHeight, densityX, densityY, press, pressTop, sparseWidth, sparseTop, rveHeight)
+save(block, "sparse", sqrt(blockWidth*blockHeight)*coarse, refineTimes)
 
+plt.show()
